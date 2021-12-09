8e788d1fcdd2b8f491a78a0aa02303c62bf12452
vasif-student <vasifah@code.edu.az>
2021-12-09 18:59:54 +0400

    ajax

---
+++ ajaxPartialProj/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using ajaxPartialProj.DAL;
+using ajaxPartialProj.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ajaxPartialProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(new HomeIndexViewModel { 
+                Products = _context.Products.Include(p => p.Category).ToList(),
+                Categories = _context.Categories.FirstOrDefault()
+            });
+        }
+    }
+}
---
+++ ajaxPartialProj/ViewModels/ProductIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ajaxPartialProj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ajaxPartialProj.ViewModels
+{
+    public class ProductIndexViewModel
+    {
+        public List<Product> Products { get; set; }
+        public Category Categories { get; set; }
+    }
+}
